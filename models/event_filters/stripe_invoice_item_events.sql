378d68be7142504020a751238dc1f6e5e08a58be
Tristan Handy <jthandy@gmail.com>
2017-02-21 21:49:54 -0500

    additional fields on invoice_items

--- models/event_filters/stripe_invoice_item_events.sql
+++ models/event_filters/stripe_invoice_item_events.sql
@@ -9,6 +9,7 @@ select
     data__object__id as id,
     nullif(data__object__invoice, '') as invoice_id,
     nullif(data__object__customer, '') as customer_id,
+    nullif(data__object__subscription, '') as subscription_id,
     nullif(id, '') as event_id,
 
     "type" as event_type,
@@ -16,6 +17,9 @@ select
     data__object__date as invoice_date,
     data__object__period__start as period_start,
     data__object__period__end as period_end,
+
+    data__object__proration as proration,
+    data__object__plan__id as plan_id,
 
     data__object__amount as amount,
     data__object__currency as currency,
