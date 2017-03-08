82ec44128d96133f85c18404b431e37b4fbcaaec
Tristan Handy <jthandy.gmail.com>
2017-03-07 21:55:47 -0500

    reworked discount events to fit into new structure

--- models/core_entities/stripe_discounts.sql
+++ models/core_entities/stripe_discounts.sql
@@ -1,37 +1,27 @@
-with discount_events as (
+{% set partition_clause = "partition by id order by created_at rows between unbounded preceding and unbounded following" %}
 
-  select * from {{ref('stripe_discount_events')}}
+with events as (
 
-), days as (
-
-  select * from {{ref('days')}}
+    select * from {{ref('stripe_discount_events')}}
 
 )
 
 select distinct
-  customer_id,
-  date_day,
-  last_value(coupon_id) over
-    (partition by customer_id, date_day
-      order by created_at
-      rows between unbounded preceding and unbounded following
-    ) as coupon_id,
-  last_value(discount_type) over
-    (partition by customer_id, date_day
-      order by created_at
-      rows between unbounded preceding and unbounded following
-    ) as discount_type,
-  last_value(discount_value) over
-    (partition by customer_id, date_day
-      order by created_at
-      rows between unbounded preceding and unbounded following
-    ) as discount_value,
-  first_value(created_at) over
-    (partition by customer_id, date_day
-      order by created_at
-      rows between unbounded preceding and unbounded following
-    ) as created_at
-from discount_events
-  inner join days
-    on discount_events.discount_start::date <= days.date_day
-    and (discount_events.discount_end::date > days.date_day or discount_events.discount_end is null)
+    id,
+
+    last_value(customer_id) over ( {{ partition_clause }} ) as customer_id,
+    last_value(coupon_id) over ( {{ partition_clause }} ) as coupon_id,
+    last_value(discount_type) over ( {{ partition_clause }} ) as discount_type,
+    last_value(discount_value) over ( {{ partition_clause }} ) as discount_value,
+    last_value(discount_start) over ( {{ partition_clause }} ) as discount_start,
+    last_value(discount_end) over ( {{ partition_clause }} ) as discount_end,
+
+    first_value(created_at) over ( {{ partition_clause }} ) as created_at,
+
+    min(
+        case when event_type = 'customer.discount.deleted'
+        then created_at
+        end
+    ) over ( {{ partition_clause }} ) as deleted_at
+
+from events
--- models/event_filters/stripe_discount_events.sql
+++ models/event_filters/stripe_discount_events.sql
@@ -1,68 +1,34 @@
 with events as (
 
-  select * from {{ref('stripe_events')}}
+    select * from {{ref('stripe_events')}}
 
-), discount_events as (
+),
 
-  select
-    data__object__id as id,
-    created as created_at,
-    "type" as event_type,
-    data__object__customer as customer_id,
-    data__object__coupon__id as coupon_id,
-    case
-      when data__object__coupon__percent_off is null
-        then 'amount'
-      else 'percent'
-    end as discount_type,
-    coalesce(data__object__coupon__percent_off, data__object__coupon__amount_off) as discount_value,
-    data__object__start as discount_start,
-    data__object__end as discount_end
-  from events
-  where "type" like 'customer.discount.%'
+final as (
 
-), d1 as (
-  --need to add a field here to be used in a later query.
-  select
-    *,
-    lag(discount_end, 1) over
-      (partition by customer_id
-       order by created_at
-      ) as prior_discount_end
-  from discount_events
+    select
+
+        data__object__id as id,
+        created as created_at,
+        "type" as event_type,
+        data__object__customer as customer_id,
+        data__object__coupon__id as coupon_id,
+
+        case
+            when data__object__coupon__percent_off is null then 'amount'
+            else 'percent'
+        end as discount_type,
+
+        coalesce(data__object__coupon__percent_off,
+                data__object__coupon__amount_off) as discount_value,
+
+        data__object__start as discount_start,
+        data__object__end as discount_end
+
+    from events
+
+    where "type" like 'customer.discount.%'
 
 )
 
---this is kinda shitty to have to do, but there are instances where there are duplicate records that are *exactly* the same
---but have different created_at's. this query de-dupes them.
-select
-  id,
-  event_type,
-  customer_id,
-  coupon_id,
-  discount_type,
-  discount_value,
-  discount_start,
-  discount_end,
-  min(created_at) as created_at
-from d1
-where event_type != 'customer.discount.deleted'
-group by 1, 2, 3, 4, 5, 6, 7, 8
-
-union all
-
---need to union together discount deleted entries because their date fields don't really support the logic we need
---to do in subsequent amortization queries. we need the discount to look cancelled for the entire remaining duration
---of the original discount.
-select
-  id,
-  event_type,
-  customer_id,
-  coupon_id,
-  discount_type,
-  0::numeric(38,6) as discount_value,
-  discount_end as discount_start,
-  prior_discount_end as discount_end,
-  created_at
-from d1
-where event_type = 'customer.discount.deleted'
+select * from final
