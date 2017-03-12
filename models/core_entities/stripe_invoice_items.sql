0e2ad03be9ec274b240081d87d0904ea173d362f
Tristan Handy <jthandy.gmail.com>
2017-03-12 18:20:27 -0400

    fixed issues with nulls in certain fields in invoiceitems

--- models/core_entities/stripe_invoice_items.sql
+++ models/core_entities/stripe_invoice_items.sql
@@ -9,18 +9,18 @@ with events as (
 select distinct
     id,
 
-    last_value(invoice_id) over ( {{ partition_clause }} ) as invoice_id,
+    last_value(invoice_id ignore nulls) over ( {{ partition_clause }} ) as invoice_id,
     last_value(customer_id) over ( {{ partition_clause }} ) as customer_id,
     last_value(event_id) over ( {{ partition_clause }} ) as event_id,
-    last_value(subscription_id) over ( {{ partition_clause }} ) as subscription_id,
+    last_value(subscription_id ignore nulls) over ( {{ partition_clause }} ) as subscription_id,
     last_value(invoice_date) over ( {{ partition_clause }} ) as invoice_date,
-    last_value(period_start) over ( {{ partition_clause }} ) as period_start,
-    last_value(period_end) over ( {{ partition_clause }} ) as period_end,
-    last_value(proration) over ( {{ partition_clause }} ) as proration,
-    last_value(plan_id) over ( {{ partition_clause }} ) as plan_id,
-    last_value(amount) over ( {{ partition_clause }} ) as amount,
-    last_value(currency) over ( {{ partition_clause }} ) as currency,
-    last_value(description) over ( {{ partition_clause }} ) as description,
+    last_value(period_start ignore nulls) over ( {{ partition_clause }} ) as period_start,
+    last_value(period_end ignore nulls) over ( {{ partition_clause }} ) as period_end,
+    last_value(proration ignore nulls) over ( {{ partition_clause }} ) as proration,
+    last_value(plan_id ignore nulls) over ( {{ partition_clause }} ) as plan_id,
+    last_value(amount ignore nulls) over ( {{ partition_clause }} ) as amount,
+    last_value(currency ignore nulls) over ( {{ partition_clause }} ) as currency,
+    last_value(description ignore nulls) over ( {{ partition_clause }} ) as description,
 
     first_value(created_at) over ( {{ partition_clause }} ) as created_at,
 
