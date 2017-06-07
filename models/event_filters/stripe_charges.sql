e0014fc91250cb3537289b753336dce710f3445c
Ted Conbeer <tconbeer@users.noreply.github.com>
2017-06-07 15:00:04 -0400

    Adds FK to balance transaction
    The balance transaction object contains information on the stripe fee collected with this charge, which is very handy for cash reconciliation

--- models/event_filters/stripe_charges.sql
+++ models/event_filters/stripe_charges.sql
@@ -8,6 +8,7 @@ select
   id,
   data__object__customer as customer_id,
   data__object__invoice as invoice_id,
+  data__object__balance_transaction as balance_transaction_id,
   data__object__amount as amount,
   trim('charge.' from "type") as result,
   data__object__created as created_at
