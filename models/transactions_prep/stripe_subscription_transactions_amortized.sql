2fc4f9f24d2e7e9949e561b7bb251d7db0fdc067
Erin Ogilvy <erin.ogilvy@gmail.com>
2018-01-24 08:32:00 -0500

    altering annual proration rollup

--- models/transactions_prep/stripe_subscription_transactions_amortized.sql
+++ models/transactions_prep/stripe_subscription_transactions_amortized.sql
@@ -4,133 +4,59 @@
   sort = "transaction_date"
 ) }}
 
+--this model rolls up the daily amortization of invoices to the last value for
+--a customer in a given month
+
 with transactions as (
 
-    select * from {{ref('stripe_subscription_transactions_unioned')}}
+    select * from {{ref('stripe_subscription_transactions_amortized_daily')}}
 
 ),
 
-months as (
+rollup as (
 
-    select * from {{ref('months')}}
+    select distinct
 
-),
-
-amortized as (
-
-    select
-
-        transactions.*,
-
-        dateadd(
-            month,
-            datediff(month, date_trunc('month', transactions.period_start),
-            date_month),
-            invoice_date
-        ) as transaction_date
-
-
-    from transactions
-
-    inner join months
-        on date_trunc('month', transactions.period_start) <= months.date_month
-        and date_trunc('month', transactions.period_end) > months.date_month
-
-    where diff > 0
-
-),
-
-combined as (
-
-    select * from amortized
-
-    union all
-
-    select *, invoice_date as transaction_date
-    from transactions
-    where diff = 0
-
-),
-
-final as (
-
-    select
-
-        source_item_type,
-        source_item_id,
-        subscription_id,
+        last_value(source_item_type) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as source_item_type,
+        last_value(source_item_id) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as source_item_id,
+        last_value(subscription_id) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as subscription_id,
         customer_id,
-        transaction_date,
-        invoice_date,
-        period_start,
-        period_end,
+        max(transaction_date) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as transaction_date,
+        max(invoice_date) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as invoice_date,
+        max(period_start) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as period_start,
+        max(period_end) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as period_end,
+        sum(case when customer_last_month_value = 1 then mrr_amount else null end)
+            over (partition by customer_id, date_month order by transaction_date
+            rows between unbounded preceding and unbounded following) as amount,
+        last_value(plan_id) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as plan_id,
+        last_value(forgiven) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as forgiven,
+        last_value(paid) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as paid,
+        last_value(duration) over (partition by customer_id, date_month
+            order by transaction_date rows between unbounded preceding and unbounded
+            following) as duration
 
-        case
-            when duration = 'month' then amount
-
-            --for first row of negative prorations for annual accounts, this
-            --calculates the amount of $ already used
-            when duration = 'year'
-            and source_item_type = 'proration item'
-            and amount < 0
-            and row_number() over (partition by customer_id, source_item_id
-                order by transaction_date) = 1
-
-                then ((prior_period_amount /
-                datediff('month', prior_period_start, prior_period_end)) -
-                (prior_period_amount + amount)) *-1
-
-            --for subsequent rows of negative annual prorations, it is important
-            --to simply null out the remainder of the MRR
-            when duration = 'year'
-            and source_item_type = 'proration item'
-            and amount < 0
-            and row_number() over (partition by customer_id, source_item_id
-                order by transaction_date) > 1
-                then (prior_period_amount /
-                    datediff('month', prior_period_start, prior_period_end)) * -1
-
-            --for first row of positive prorations for annual accounts, this
-            --calculates the amount for the first month
-
-            when duration = 'year'
-            and source_item_type = 'proration item'
-            and amount > 0
-            and row_number() over (partition by customer_id, source_item_id
-                order by transaction_date) = 1
-                then amount / datediff('second', period_start, period_end) --amount per second
-                * datediff('second', period_start,
-                to_date((
-                        date_part(month,dateadd('month', 1, period_start))::varchar
-                        || '/' ||
-                        date_part(day, period_end)::varchar || '/' ||
-                        date_part(year, period_start)::varchar
-                    ), 'mm/dd/yyyy'))
-
-
-            --for subsequent rows of positive annual prorations, it is important
-            --to figure out what the annualized value would have been
-            when duration = 'year'
-            and source_item_type = 'proration item'
-            and amount > 0
-            and row_number() over (partition by customer_id, source_item_id
-                order by transaction_date) > 1
-
-                then amount / datediff('second', period_start, period_end) --amount per second
-                * 31622400 / 12 --need to figure out leap year
-
-            when duration = 'year' then amount /
-                datediff(month, period_start, period_end)
-        end as amount,
-
-        plan_id,
-        forgiven,
-        paid,
-        duration
-
-    from combined
-
+    from transactions
 
 )
 
-select * from final
+select * from rollup
--- models/transactions_prep/stripe_subscription_transactions_unioned.sql
+++ models/transactions_prep/stripe_subscription_transactions_unioned.sql
@@ -29,53 +29,6 @@ unioned as (
     select *
     from prorations
 
-),
-
-ordered as (
-
-    select
-        *,
-        dense_rank() over (partition by customer_id order by period_start)
-            as customer_start_rank
-    from unioned
-    order by period_start, amount
-
-),
-
-calculate as (
-
-    select
-        *,
-        date_diff('month', period_start, period_end) as diff,
-        case
-            when source_item_type = 'proration item'
-            and duration = 'year'
-                then last_value(case when amount > 0
-                    and duration = 'year' then amount else null end ignore nulls)
-                    over (partition by customer_id order by customer_start_rank, amount
-                    rows between unbounded preceding and 1 preceding)
-            else null
-        end as prior_period_amount,
-        case
-            when source_item_type = 'proration item'
-            and duration = 'year'
-                then last_value(case when amount > 0
-                    and duration = 'year' then period_start else null end ignore nulls)
-                    over (partition by customer_id order by customer_start_rank, amount
-                    rows between unbounded preceding and 1 preceding)
-            else null
-        end as prior_period_start,
-        case
-            when source_item_type = 'proration item'
-            and duration = 'year'
-                then last_value(case when amount > 0
-                    and duration = 'year' then period_end else null end ignore nulls)
-                    over (partition by customer_id order by customer_start_rank, amount
-                    rows between unbounded preceding and 1 preceding)
-            else null
-        end as prior_period_end
-    from ordered
-
 )
 
-select * from calculate
+select * from unioned
